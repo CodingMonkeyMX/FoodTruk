f35af614501b56689431263446d7c2749b5d899c
CodingMonkeyMX <ebotking11@gmail.com>
2025-11-18 13:19:02 -0500

    commit

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-
-    Desserts cheeseCake = new Desserts("CheeseCake",6.49,"Small");
-    Drinks cocaCola = new Drinks("CocaCola",6.49,"Large");
-    MainCourse pizza = new MainCourse("Pizza",6.49,"Medium");
-}
