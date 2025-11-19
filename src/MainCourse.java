54d06bd00e262dc122c89b626d19f5100ca8b95c
kohal2 <kieranoohalloran@gmail.com>
2025-11-19 14:22:31 -0500

    MainCourse

--- src/MainCourse.java
+++ src/MainCourse.java
@@ -2,14 +2,11 @@ public class MainCourse extends TestFood /*implements Dishterface*/ {
 
 
     // variables
-    public static int numMains = 0;
+    private Display display;
 
-
-    public MainCourse(String name, double cost, String size) {
-        super(cost,name,size);
-        numMains++;
+    private String[] MainCourse = {"Pizza", "Burger", "Steak"};
+    private double[] prices = {19.49, 15.99, 33.49};
+    public MainCourse(Display display) {
+        this.display = display;  // Store reference to GUI
     }
-
-
 }
-
