0a0de62437b2f942c4f1588967db985f679c91e7
CodingMonkeyMX <ebotking11@gmail.com>
2025-11-19 13:46:40 -0500

    commit

--- src/Entrees.java
+++ src/Entrees.java
@@ -6,8 +6,5 @@ public class Entrees /*implements Dishterface*/ {
     private String[] entrees = {"hot dog", "elote", "Wonton soup"};
     private double[] prices = {3.99, 4.99, 7.99};
 
-    public Entrees(Display display) {
-        this.display = display;  // Store reference to GUI
-    }
 
 }
