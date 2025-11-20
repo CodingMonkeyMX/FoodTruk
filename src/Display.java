82805400534800d47284c5fcab51f7e34e7dace2
kiwioh-code <kieranoohalloran@gmail.com>
2025-11-20 12:44:49 -0500

    sigma

--- src/Display.java
+++ src/Display.java
@@ -6,6 +6,8 @@ public class Display extends JFrame {
     private JLabel order;
     private JLabel title;
     private JPanel menuPanel;
+    private JPanel buttonBar;
+    private JTextPane receipt;
     public Display() {
         // initializing GUI's
             setBounds(0, 0, 900, 600);
@@ -22,6 +24,18 @@ public class Display extends JFrame {
             menuPanel.setLayout(new GridLayout(4,1));
             menuPanel.setBorder(BorderFactory.createLineBorder(Color.black));
             this.add(menuPanel);
+
+        receipt = new JPanel();
+        menuPanel.setBounds(50,100,400,400);
+        menuPanel.setLayout(new GridLayout(4,1));
+        menuPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        this.add(menuPanel);
+
+        menuPanel = new JPanel();
+        menuPanel.setBounds(50,505,800,52);
+        menuPanel.setLayout(new FlowLayout());
+        menuPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        this.add(menuPanel);
 /*      // text input area
             dogDisplay = new JTextArea(giveDogDisplay(1));
             dogDisplay.setBounds(50, 50, 400, 350);
