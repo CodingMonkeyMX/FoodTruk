6687f1f26f50a7da0d8e7f834932f93ca0cd341c
kiwioh-code <kieranoohalloran@gmail.com>
2025-11-21 14:49:43 -0500

    UI

---
+++ src/MenuFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuFrame {
+    private JFrame menuFrame;
+
+    private JPanel leftPanel;
+    private JPanel middlePanel;
+    private JPanel rightPanel;
+
+    private JLabel
+
+    public MenuPanel(){
+
+    }
+}
+
